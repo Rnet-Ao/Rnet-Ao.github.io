ac510032ee9f4867896e164452ec2f4101f09da3
Rnet-Ao <Rnet_Ao@foxmail.com>
2020-05-13 14:22:47 +0800

    chore: init

---
+++ precache-manifest.7c93b4e26bb7d24b20b611e32026037a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d8ed230f270566d5decc2ca901be04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c93649bf51712b324c5",
+    "url": "/static/css/main.d65c6c31.chunk.css"
+  },
+  {
+    "revision": "d64f18feee73ee968e24",
+    "url": "/static/js/2.48291014.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.48291014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c93649bf51712b324c5",
+    "url": "/static/js/main.c3abb75d.chunk.js"
+  },
+  {
+    "revision": "34a23f0ee4a1ee7d0d48",
+    "url": "/static/js/runtime-main.e5b1187f.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/static/media/iconfont.3a2ba315.ttf"
+  },
+  {
+    "revision": "9ec5e40edddff9ff300e6791941fe869",
+    "url": "/static/media/iconfont.9ec5e40e.eot"
+  },
+  {
+    "revision": "ca5d4588dad9c32f49e895e7f19479df",
+    "url": "/static/media/iconfont.ca5d4588.svg"
+  }
+]);
